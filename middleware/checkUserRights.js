c204abe865e9938550601f23a14fedce2d7da17a
PrinceSvelte <prince.rajput@sveltetech.com>
2023-05-01 14:17:53 +0530

    added next controller in middleware

--- middleware/checkUserRights.js
+++ middleware/checkUserRights.js
@@ -2,6 +2,10 @@ const Todo = require('../models/Todos')
 
 const checkUserRights = async function(req,res,next) {
     const {id} = req.query
+    if(!id){
+       next()
+       return
+    }
     try {
         const todo = await Todo.findById(id)
         if(!todo){
@@ -11,9 +15,9 @@ const checkUserRights = async function(req,res,next) {
             return res.status(401).send('You dont have permission to operate this todo !');
         }else {
             next()
-        } 
+        }
     } catch (error) {
-        return res.status(404).json({ error });  
+        return res.status(404).json({ error }); 
     }
 
 }
